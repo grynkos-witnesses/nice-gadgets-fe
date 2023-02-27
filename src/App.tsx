76c292de6ad8d2263a6dd8966afc417f056cfa96
Roman Bliashuk <romanblyashuk94@gmail.com>
2023-02-28 01:47:05 +0200

    added mock header

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import './App.scss';
 import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
 
 export const App: React.FC = () => {
   return (
     <div className="App">
+      <Header />
       <main className="App_content">
         <Outlet />
       </main>
