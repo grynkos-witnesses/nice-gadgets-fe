bd16e30d70796aec45afe3092e33d5f35038b17a
Yana Gonchar <johanna.gonchar@icloud.com>
2023-02-28 22:18:11 +0100

    Cart Layout

---
+++ src/pages/CartPage.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react';
+import { Cart } from '../components/Cart';
+
+export {} from '../components/Cart';
+
+export const CartPage: React.FC = () => {
+  return (
+    <div className="cart__page">
+      <Cart />
+    </div>
+  );
+};
