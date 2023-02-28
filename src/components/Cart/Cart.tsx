aae4a8705bf692c4631e5711644f75521d70a642
Yana Gonchar <johanna.gonchar@icloud.com>
2023-02-28 22:18:11 +0100

    Cart Layout

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -7,6 +7,7 @@ import CartSCSS from './Cart.module.scss';
 
 export const Cart: React.FC = () => {
   return (
+<<<<<<< HEAD
     <div className={CartSCSS.cart}>
       <Link to="/" className={CartSCSS.cart__back_content}>
         <img src={back} alt="back icon" className={CartSCSS.cart__link} />
@@ -39,6 +40,50 @@ export const Cart: React.FC = () => {
             Checkout
           </button>
         </div>
+=======
+    <div className={CartSCSS.cart__page}>
+      <Link to="/" className={CartSCSS.cart__page__back_content}>
+        <img
+          src={back}
+          alt="back icon"
+          className={CartSCSS.cart__page__link}
+        />
+        <span className={CartSCSS.cart__page__back_text}>Back</span>
+      </Link>
+
+      <h1 className={CartSCSS.cart__page__header}>Cart</h1>
+
+      {/* CartItem will be here */}
+
+      <div>
+        <div className={CartSCSS.cart__page__card_item}>
+          Card Item 1
+        </div>
+
+        <div className={CartSCSS.cart__page__card_item}>
+          Card Item 2
+        </div>
+
+        <div className={CartSCSS.cart__page__card_item}>
+          Card Item 3
+        </div>
+      </div>
+
+      <div className={CartSCSS.cart__page__checkout}>
+        <span className={CartSCSS.cart__page__checkout_price}>$2657</span>
+        <span
+          className={CartSCSS.cart__page__checkout_total}
+        >
+          Total for 3 items
+        </span>
+        <div className={CartSCSS.cart__page__checkout_pipe} />
+        <button
+          type="button"
+          className={CartSCSS.cart__page__checkout_button}
+        >
+          Checkout
+        </button>
+>>>>>>> 8b033ea (Cart Layout)
       </div>
     </div>
   );
