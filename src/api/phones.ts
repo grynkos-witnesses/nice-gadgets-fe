96aaa1ad27e49ff4838b55f2286b0826c95baa75
Oleh <lifserv@gmail.com>
2023-03-04 14:52:57 +0200

    One more minor fix

--- src/api/phones.ts
+++ src/api/phones.ts
@@ -6,17 +6,26 @@ axios.defaults.baseURL = 'https://secret-meadow-92340.herokuapp.com/';
 export function getPhones(page: number, perPage: number) {
   return axios
     .get(`/phones?page=${page}&perPage=${perPage}`)
-    .then((res) => res.data);
+    .then((res) => res.data)
+    .catch((err) => {
+      throw new Error(`${err}`);
+    });
 }
 
 // export function getAll(): Promise<Phone[]> {
 //   return axios
 //     .get('/phones')
-//     .then(res => res.data);
+//     .then(res => res.data)
+//     .catch((err) => {
+//       throw new Error(`${err}`);
+//     });
 // }
 
 // export function getOne(phoneId: string): Promise<Phone> {
 //   return axios
 //     .get(`/phones/${phoneId}`)
-//     .then(res => res.data);
+//     .then(res => res.data)
+//     .catch((err) => {
+//       throw new Error(`${err}`);
+//     });
 // }
