fe36246065b0f6835b80aeaa8d03e05b6059b6c9
Roman Bliashuk <romanblyashuk94@gmail.com>
2023-03-06 16:21:14 +0200

    Small fixes of main banner

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -5,11 +5,15 @@ import s from './HomePage.module.scss';
 export const HomePage = () => {
   return (
     <div className={s.homePage}>
-      <div className={s.container}>
+      <div className="container">
         <h1 className={s.title}>Welcome to Nice Gadgets store!</h1>
+      </div>
 
+      <div className={s.homePage__mainSlider}>
         <Swiper />
       </div>
+
+      <div className="container">Bla-bla-blas</div>
     </div>
   );
 };
