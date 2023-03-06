db37f50d7fecf6c900d1fd4215f84493884f2491
Yana Eva Gonchar <johanna.gonchar@icloud.com>
2023-03-06 13:57:41 +0100

    Slider

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Swiper } from '../../components/Swiper';
 import s from './HomePage.module.scss';
 
 export const HomePage = () => {
   return (
     <div className={s.homePage}>
-      <div className="container">
-        <h1 className="page__title">Welcome to Nice Gadgets store!</h1>
+      <div className={s.container}>
+        <h1 className={s.title}>Welcome to Nice Gadgets store!</h1>
+
+        <Swiper />
       </div>
     </div>
   );
