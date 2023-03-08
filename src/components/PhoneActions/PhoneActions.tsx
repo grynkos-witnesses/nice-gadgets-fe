c7f6a92e9baab055c04710ea9d9e1d8b0f35aedd
Artem Rymarchuk <rumarchyk204@gmail.com>
2023-03-08 14:37:03 +0100

    commit before rebase

---
+++ src/components/PhoneActions/PhoneActions.tsx
@@ -0,0 +1,8 @@
+import React, { memo } from 'react';
+import s from './PhoneActions.module.scss';
+
+export const PhoneActions: React.FC = memo(() => {
+  return (
+    <div className={s.container}></div>
+  );
+});
